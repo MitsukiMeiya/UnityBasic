42570b43b79d64008af8c69fd3d6a37aac1035e1
MitsukiMeiya <cldus26@gmail.com>
2025-04-16 15:39:21 +0900

    TankMove

---
+++ Assets/Imports/Tank/Scenes/TankMover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        float input = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * moveSpeed * input * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        float input = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, rotateSpeed * input * Time.deltaTime);
+    }
+}
