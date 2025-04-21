a63c944361790c2100d476a3447f7f696343b115
MitsukiMeiya <cldus26@gmail.com>
2025-04-21 09:31:20 +0900

    250417

--- Assets/Imports/Tank/Scenes/Shooter.cs
+++ Assets/Imports/Tank/Scenes/Shooter.cs
@@ -6,14 +6,15 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Transform muzzlePoint;
-    [SerializeField] ObjectPool bulletPool;
+    //[SerializeField] ObjectPool bulletPool;
 
     [Range(10,30)]
     [SerializeField] float bulletSpeed;
 
     public void Fire()
     {
-        PooledObject instance = bulletPool.GetPool(muzzlePoint.position, muzzlePoint.rotation);
+        gameObject instance = Instantiate(bulletPrefab.);
+        //PooledObject instance = bulletPool.GetPool(muzzlePoint.position, muzzlePoint.rotation);
         Rigidbody bulletRigidbody = instance.GetComponent<Rigidbody>();
         bulletRigidbody.velocity = muzzlePoint.forward * bulletSpeed;
     }
